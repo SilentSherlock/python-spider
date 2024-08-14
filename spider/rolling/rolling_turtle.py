438a59ab2279fc7598ea5720f58fe1c1e61997aa
SilentSherlock <1536141515@qq.com>
2024-08-14 16:22:12 +0800

    1.rolling stone sailing

--- spider/rolling/rolling_turtle.py
+++ spider/rolling/rolling_turtle.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, font
 import turtle
 import random
 from PIL import Image, ImageTk, ImageSequence
@@ -22,7 +22,7 @@ class DiceRollerApp:
         # Create turtle for drawing
         self.t = turtle.RawTurtle(self.screen)
         self.t.shape("turtle")
-        self.t.hideturtle()
+        self.t.color()
 
         # Create gif resource
         image_src = Image.open("../../resource/image/diceRollingByFa.gif")
@@ -46,15 +46,18 @@ class DiceRollerApp:
         # Create start and accept button, start for roll, accept for change image
         self.start_button = ttk.Button(root, text="Start Rolling", command=self.start_roll)
         self.start_button.grid(row=2, column=1, columnspan=2)
-        self.accept_button = ttk.Button(root, text="Accept")
+        self.accept_button = ttk.Button(root, text="Accept", command=self.accept_roll)
         self.accept_button.grid(row=3, column=1, columnspan=2)
 
         # Draw initial stick figure with dice
         self.draw_star_figure()
 
-    def draw_star_figure(self, angle=0):
-        # Draw Slogan
-        return
+    def draw_star_figure(self, text="Rolling Life Style"):
+        # 使用font.Font只能加载系统字体，无法加载给定字体
+        font_path = "../../resource/font/Caveat-VariableFont_wght.ttf"
+        custom_font = font.Font(font="Consolas", size=20)
+        self.canvas.create_text(-100, 100, anchor=tk.NW, text=text, font=custom_font)
+        # print(font.families())
 
     def start_roll(self):
         self.t.clear()
@@ -68,6 +71,13 @@ class DiceRollerApp:
         self.topic_entry.insert(0, "fuck")
         self.activity_entry.insert(0, "shit")
         self.canvas_image_flag = False
+
+    def accept_roll(self):
+        image_accept = Image.open("../../resource/image/clapFa.gif_s200x0")
+        image_accept_frames = []
+        for frame in ImageSequence.Iterator(image_accept):
+            image_accept_frames.append(ImageTk.PhotoImage(frame))
+        self.frames = image_accept_frames
 
     def load_image(self, frame_index=0):
         if self.canvas_image_flag:
