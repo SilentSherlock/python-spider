0aa918f4b685f2cf4b779a967e6124b59371ff80
SilentSherlock <1536141515@qq.com>
2025-08-31 13:11:39 +0800

    1.优化刷量脚本

--- okx_exchange/okx_trend_trade_strategy_bot.py
+++ okx_exchange/okx_trend_trade_strategy_bot.py
@@ -2,14 +2,10 @@ import datetime
 import threading
 import time
 
-import numpy as np
-from enums.RequestEnums import OrderType
-from okx import Account, Trade, Funding, PublicData, MarketData
-
-from arbitrage_bot.backpack_okx_arbitrage_bot import close_backpack_position_by_order_id, \
-    SYMBOL_OKX_INSTRUMENT_MAP, calc_qty, execute_okx_order_swap, close_okx_position_by_order_id
-from backpack_exchange.trade_prepare import proxy_on, load_okx_api_keys_trade_cat_okx_trend, okx_account_api_test, \
-    okx_trade_api_test, okx_market_api_test
+from arbitrage_bot.backpack_okx_arbitrage_bot import SYMBOL_OKX_INSTRUMENT_MAP, calc_qty, execute_okx_order_swap, \
+    close_okx_position_by_order_id
+from backpack_exchange.trade_prepare import proxy_on, okx_account_api_test, \
+    okx_trade_api_test, okx_market_api_test, okx_market_api, okx_account_api, okx_trade_api
 from okx_exchange.macd_signal import macd_signals
 from utils.logging_setup import setup_logger, setup_okx_macd_logger
 
@@ -17,15 +13,6 @@ from utils.logging_setup import setup_logger, setup_okx_macd_logger
 proxy_on()
 logger = setup_logger(__name__)
 okx_trade_macd_logger = setup_okx_macd_logger()
-
-okx_live_trading = "0"
-OKX_API_KEY, OKX_SECRET_KEY, OKX_PASSPHRASE = load_okx_api_keys_trade_cat_okx_trend()
-okx_account_api = Account.AccountAPI(
-    OKX_API_KEY, OKX_SECRET_KEY, OKX_PASSPHRASE, False, okx_live_trading)
-okx_trade_api = Trade.TradeAPI(OKX_API_KEY, OKX_SECRET_KEY, OKX_PASSPHRASE, False, okx_live_trading)
-okx_funding_api = Funding.FundingAPI(OKX_API_KEY, OKX_SECRET_KEY, OKX_PASSPHRASE, False, okx_live_trading)
-okx_public_api = PublicData.PublicAPI(OKX_API_KEY, OKX_SECRET_KEY, OKX_PASSPHRASE, False, okx_live_trading)
-okx_market_api = MarketData.MarketAPI(OKX_API_KEY, OKX_SECRET_KEY, OKX_PASSPHRASE, False, okx_live_trading)
 
 SYMBOL = "SOL-USDT-SWAP"
 TREND_SYMBOL_LIST = [
@@ -43,67 +30,7 @@ LOSS_LIMIT = 0.2  # 亏损20%止损
 PROFIT_DRAWBACK = 0.2  # 盈利回撤20%止盈保护
 
 
-def monitor_position(okx_price, direction, order_id, okx_qty, leverage=LEVERAGE, monitor_symbol=SYMBOL):
-    peak_price = okx_price
-    price_history = [okx_price]
-
-    while True:
-        time.sleep(60)
-        okx_ticker = okx_market_api.get_ticker(instId=monitor_symbol)
-        current_price = float(
-            okx_ticker["data"][0]["last"]) if okx_ticker and "data" in okx_ticker else None
-        price_history.append(current_price)
-        if len(price_history) > 6:
-            price_history.pop(0)
-
-        # 加上杠杆计算实际盈亏比例
-        pnl = ((current_price - okx_price) / okx_price * leverage) if direction == 'long' \
-            else ((okx_price - current_price) / okx_price * leverage)
-
-        if direction == 'long':
-            peak_price = max(peak_price, current_price)
-        else:
-            peak_price = min(peak_price, current_price)
-
-        # 加上杠杆计算实际回撤比例
-        draw_down = ((peak_price - current_price) / peak_price * leverage) if direction == 'long' \
-            else ((current_price - peak_price) / peak_price * leverage)
-
-        print(
-            f"当前{monitor_symbol}价格: {current_price:.4f}, 下单价格:{okx_price}, direction: {direction} "
-            f"杠杆盈亏: {pnl:.4%}, 杠杆回撤: {draw_down:.2%}")
-
-        if pnl <= -0.2:
-            print(f"止损触发，亏损金额: {(okx_price - current_price) * float(okx_qty):.4f} USDC")
-            break
-        if pnl > 0 and draw_down >= 0.3:
-            print(f"盈利回撤触发，当前盈利: {abs((current_price - okx_price)) * float(okx_qty):.4f} USDC")
-            break
-
-        # 判断最近六个价格的趋势
-        if len(price_history) == 6:
-            x = list(range(6))
-            y = price_history
-            # 线性拟合，获取斜率
-            k, _ = np.polyfit(x, y, 1)
-            # 判断趋势与方向相反
-            if (direction == 'long' and k < 0) or (direction == 'short' and k > 0):
-                print("最近六次价格趋势与持仓方向相反，平仓")
-                break
-            # 判断趋势趋向于直线（斜率接近0）
-            if abs(k) < 0.001:
-                print("最近六次价格曲线趋向于直线，平仓")
-                break
-    # 关仓
-    # 计算盈亏
-    profit = float(okx_qty) * (current_price - okx_price) if direction == 'long' \
-        else float(okx_qty) * (okx_price - current_price)
-    print(f"准备平仓: {monitor_symbol}, 方向: {direction}, 数量: {okx_qty}, "
-          f"盈亏: {profit :.4f} USDC")
-    close_backpack_position_by_order_id(monitor_symbol, order_id, okx_qty)
-
-
-def fetch_kline_data(market_api=okx_market_api, kline_symbol=SYMBOL, interval="5m", limit=30):
+def fetch_kline_data(market_api=okx_market_api_test, kline_symbol=SYMBOL, interval="5m", limit=30):
     """
     获取指定交易对的最新已经完结limit根K线数据，返回数据由新到旧排序，最新的K可能未结束
     :param market_api:
@@ -121,7 +48,10 @@ def fetch_kline_data(market_api=okx_market_api, kline_symbol=SYMBOL, interval="5
     return klines_data
 
 
-def monitor_position_macd(direction_symbol=SYMBOL):
+def monitor_position_macd(direction_symbol=SYMBOL,
+                          account_api=okx_account_api_test,
+                          trade_api=okx_trade_api_test,
+                          market_api=okx_market_api_test):
     """
     计算指定交易对的最新MACD指标，进行开仓
     策略：
@@ -133,6 +63,9 @@ def monitor_position_macd(direction_symbol=SYMBOL):
     若开仓，进入持仓监控：
     * 进行关仓方向判断
     * 判断需要关单时，调用方法进行平仓
+    :param market_api:
+    :param trade_api:
+    :param account_api:
     :param direction_symbol:
     :return:
     """
@@ -150,7 +83,8 @@ def monitor_position_macd(direction_symbol=SYMBOL):
     while True:
         logger.info("开始新一轮信号计算")
         klines_interval = str(interval) + "m"
-        klines = fetch_kline_data(market_api=okx_market_api_test, kline_symbol=direction_symbol, interval=klines_interval, limit=50)
+        klines = fetch_kline_data(market_api=market_api, kline_symbol=direction_symbol, interval=klines_interval,
+                                  limit=50)
         macd_signal = macd_signals(klines)
 
         macd_signal_target = {}
@@ -222,8 +156,8 @@ def monitor_position_macd(direction_symbol=SYMBOL):
                     try:
                         okx_result = execute_okx_order_swap(
                             direction_symbol, direction, okx_qty, ticker_price,
-                            order_type="market", account_api=okx_account_api_test,
-                            trade_api=okx_trade_api_test, )
+                            order_type="market", account_api=account_api,
+                            trade_api=trade_api, )
                         break
                     except Exception as okx_e:
                         if attempt == 2:
@@ -247,14 +181,14 @@ def monitor_position_macd(direction_symbol=SYMBOL):
                         or macd_signal_target["zero_down"] or macd_signal_target["death_cross"]):
                     close_flag = True
             elif "short" == position.get("okx_direction"):
-                if long_signal_2 or long_signal_1 or long_signal_3 or long_signal_4 or long_signal_5\
+                if long_signal_2 or long_signal_1 or long_signal_3 or long_signal_4 or long_signal_5 \
                         or macd_signal_target["zero_up"] or macd_signal_target["golden_cross"]:
                     close_flag = True
             if close_flag:
                 close_okx_position_by_order_id(symbol=position["okx_symbol"],
                                                order_id=position["okx_order_id"],
                                                okx_qty=position["okx_qty"],
-                                               trade_api=okx_trade_api_test)
+                                               trade_api=trade_api)
                 position = None
                 logger.info("平仓完成，等待下一次开仓信号")
             else:
@@ -263,35 +197,15 @@ def monitor_position_macd(direction_symbol=SYMBOL):
         time.sleep(OKX_OPEN_INTERVAL_SEC)
 
 
-# 两根15分钟k线判断方法
-def get_open_direction_15mkline(kline_symbol=SYMBOL):
-    # 两根15分钟k线判断
-    now = time.localtime()
-    end_time = int(
-        time.mktime((now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min // 15 * 15, 0, 0, 0, -1))) * 1000
-    start_time = end_time - 2 * 15 * 60 * 1000  # 转换为毫秒
-    interval = "15m"
-    klines = okx_market_api.get_mark_price_candlesticks(
-        instId=kline_symbol, bar=interval, after=end_time, before=start_time, limit=2
-    )
-    k1, k2 = klines[-2], klines[-1]
-    up = float(k1['close']) > float(k1['open']) and float(k2['close']) > float(k2['open'])
-    down = float(k1['close']) < float(k1['open']) and float(k2['close']) < float(k2['open'])
-    print(
-        f"15分钟K线判断: symbol: {kline_symbol} k1 open:{k1['open']}, k1 close:{k1['close']}, k2 open:{k2['open']}, k2 close：{k2['close']}"
-        f", up: {up}, down: {down}")
-    if up:
-        return "long"
-    elif down:
-        return "short"
-    else:
-        return False
-
-
 if __name__ == "__main__":
     threads = []
     for SYMBOL in TREND_SYMBOL_LIST:
-        t = threading.Thread(target=monitor_position_macd, args=(SYMBOL,), name=f"Thread-{SYMBOL}")
+        t = threading.Thread(target=monitor_position_macd,
+                             args=(SYMBOL, okx_account_api_test, okx_trade_api_test, okx_market_api_test),
+                             name=f"Thread-{SYMBOL}")
+        # t = threading.Thread(target=monitor_position_macd,
+        #                      args=(SYMBOL, okx_account_api, okx_trade_api, okx_market_api),
+        #                      name=f"Thread-{SYMBOL}")
         t.start()
         time.sleep(40)
         threads.append(t)
